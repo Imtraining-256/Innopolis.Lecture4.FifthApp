86b85c9207e01afec3d33ef8ac2da0c470a17f1d
atenyaeves <atenyaev@protonmail.com>
2021-09-22 21:08:13 +0700

    Used car

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,29 @@
+import beepers.Beeper;
+import beepers.FancyMusic;
 import beepers.Horn;
+import beepers.MagicSparks;
+import engines.Engine;
 import engines.Engine12V;
+import engines.FSDEngine;
 
 public class Main {
     public static void main(String[] args) {
-        Horn horn = new Horn();
-        Engine12V engine12V = new Engine12V();
 
-        Lambo lambo = new Lambo(horn, engine12V);
+        Beeper horn = new Horn();
+        Beeper magic = new MagicSparks();
+        Beeper music = new FancyMusic();
+
+        Engine12V engine12V = new Engine12V();
+        FSDEngine fsdEngine = new FSDEngine();
+
+        Lambo lambo = new Lambo(magic, engine12V);
+        Tesla tesla = new Tesla(music, fsdEngine);
+
+        tesla.scoopFuel();
+        tesla.landToPlanet();
+
+        System.out.println(tesla.checkBeep());
+        System.out.println(tesla.checkSpeed());
 
         System.out.println(lambo.checkBeep());
         System.out.println(lambo.checkSpeed());
