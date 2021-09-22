d60298d8f15954b8d9754c5202a190c4e4b8b3a4
atenyaeves <atenyaev@protonmail.com>
2021-09-22 19:54:54 +0700

    Added abstract car

---
+++ src/main/java/Car.java
@@ -0,0 +1,21 @@
+/**
+ * Abstract Car
+ */
+public abstract class Car {
+
+    private Beeper beeper;
+    private Engine engine;
+
+    public Car(Beeper beeper, Engine engine) {
+        this.beeper = beeper;
+        this.engine = engine;
+    }
+
+    public String checkBeep(){
+        return beeper.makeSound();
+    }
+
+    public String checkSpeed(){
+        return engine.makeRotation();
+    }
+}
