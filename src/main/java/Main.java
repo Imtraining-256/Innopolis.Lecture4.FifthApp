6a5413dd0c4ace22ad2fef63745fb8b53b54a0d6
atenyaeves <atenyaev@protonmail.com>
2021-09-22 19:57:20 +0700

    Used concrete car

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Horn horn = new Horn();
+        Engine12V engine12V = new Engine12V();
 
+        Lambo lambo = new Lambo(horn, engine12V);
+
+        System.out.println(lambo.checkBeep());
+        System.out.println(lambo.checkSpeed());
     }
 }
