825ac8f95e2c7e91bf5d2339a63de2c5f0fa34de
atenyaeves <atenyaev@protonmail.com>
2021-09-22 19:55:21 +0700

    Added concrete engine and beeper

---
+++ src/main/java/Engine12V.java
@@ -0,0 +1,10 @@
+/**
+ * V12 - concrete engine makes a speed
+ */
+public class Engine12V implements Engine {
+
+    @Override
+    public String makeRotation() {
+        return "300km/h";
+    }
+}
---
+++ src/main/java/Horn.java
@@ -0,0 +1,10 @@
+/**
+ * Horn - concrete beeper makes a sound
+ */
+public class Horn implements Beeper{
+
+    @Override
+    public String makeSound() {
+        return "HOOOOOOO";
+    }
+}
